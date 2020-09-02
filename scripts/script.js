825d2fce89349bca667bcdee7d403078b2d6da05
gabriel-codes <gabrielss@outlook.com>
2020-09-02 16:11:10 -0300

    Criando Loading

--- scripts/script.js
+++ scripts/script.js
@@ -6,10 +6,24 @@ let state = {
   playlist: [],
   maxVideos: 10,
   key: '',
-  currentVideoData: {},
 };
 
-// CARREGANDO SCRIPT DE FORMA ASSÍNCRONA
+// RECUPERANDO CHAVE DA API E SALVANDO NO ESTADO
+fetch('../apiKey.json')
+  .then((response) => response.json())
+  .then((json) => (state.key = json.key));
+
+// EVENTOS DE CONTROLE DO PLAYER
+function addControls() {
+  element('play').addEventListener('click', () => player.playVideo());
+  element('next').addEventListener('click', () => player.nextVideo());
+  element('pause').addEventListener('click', () => player.pauseVideo());
+  element('prev').addEventListener('click', () => player.previousVideo());
+  element('stop').addEventListener('click', () => player.stopVideo());
+  element('controls').classList.add('controls--on');
+}
+
+// CARREGANDO SCRIPT DA API DO YOUTUBE DE FORMA ASSÍNCRONA
 let tag = document.createElement('script');
 tag.src = 'https://www.youtube.com/iframe_api';
 const firstScriptTag = document.getElementsByTagName('script')[0];
@@ -17,7 +31,7 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 // API DO YT CHAMA ASSIM QUE É CARREGADA
 function onYouTubeIframeAPIReady() {
-  // INSTÂNCIA DO PLAYER
+  // CRIA INSTÂNCIA DO PLAYER E O CARREGA NA PÁGINA
   player = new YT.Player('player', {
     width: 0,
     height: 0,
@@ -27,33 +41,19 @@ function onYouTubeIframeAPIReady() {
       playlist: '',
     },
     events: {
-      onStateChange: getCurrentVideoData,
+      onStateChange: onChangePlayer,
     },
   });
 }
 
-// EVENTOS DE CONTROLE DO PLAYER
-element('play').addEventListener('click', () => player.playVideo());
-element('next').addEventListener('click', () => player.nextVideo());
-element('pause').addEventListener('click', () => player.pauseVideo());
-element('prev').addEventListener('click', () => player.previousVideo());
-element('stop').addEventListener('click', () => player.stopVideo());
-
-// RECUPERANDO CHAVE DA API E SALVANDO NO ESTADO
-fetch('../apiKey.json')
-  .then((response) => response.json())
-  .then((json) => (state.key = json.key));
-
-// BUSCAR PLAYLIST
-const searchForm = element('searchForm');
-
-searchForm.addEventListener('submit', (event) => {
+// EVENTO DE PESQUISA DAS MÚSICAS
+element('searchForm').addEventListener('submit', (event) => {
   event.preventDefault();
   fetchVideos(state.key, event.target.formInput.value, state.maxVideos);
   console.log(player);
 });
 
-// FETCH DOS VIDEOS
+// FETCH PARA BUSCAR VIDEOS ATRAVÉS DA API DO YOUTUBE
 async function fetchVideos(key, search, maxResults) {
   const response = await fetch(
     `https://www.googleapis.com/youtube/v3/search?part=snippet&key=${key}&type=video&channelId=UCSJ4gkVC6NrvII8umztf0Ow&maxResults=${maxResults}&q=${search}`,
@@ -71,11 +71,17 @@ async function fetchVideos(key, search, maxResults) {
   }
 }
 
-// GET CURRENT VIDEO DATA
-function getCurrentVideoData(event) {
+// ON STATE PLAYER CHANGE
+function onChangePlayer(event) {
+  // MOSTRANDO LOADING ANTES DE CARREGAR INFO
+  loadingInfo(event.target.getPlayerState());
+
+  // CARREGANDO INFO E CONTROLES QUANDO REPRODUZINDO
   if (event.target.getPlayerState() == 1) {
     loadVideoThumb(event.target.getVideoData());
+    addControls();
   }
+  console.log(event.target.getPlayerState());
 }
 
 // BUSCA INVALIDA
@@ -88,7 +94,7 @@ function invalidSearch() {
   }, 1500);
 }
 
-// CARREGANDO VIDEOS
+// CARREGA OS VIDEOS NO PLAYER
 function loadVideos(playlist) {
   player.loadPlaylist(playlist);
   player.setShuffle(true);
@@ -96,19 +102,39 @@ function loadVideos(playlist) {
   player.setLoop(true);
 }
 
+// OBJETO DO NOW PLAYING
+const now = {
+  cover: element('nowCover'),
+  title: element('infoTitle'),
+  author: element('infoAuthor'),
+  time: element('infoTime'),
+  loading: element('loading'),
+};
+
+// CARREGANDO INFORMAÇÕES DA MÚSICA ATUAL
 function loadVideoThumb(data) {
   const date = new Date(player.getDuration() * 1000);
 
-  element('nowCover').src = `https://img.youtube.com/vi/${data.video_id}/0.jpg`;
-  element('infoTitle').innerText = data.title;
-  element('infoAuthor').innerText = data.author;
-  element('infoTime').innerText = `${date
+  now.cover.src = `https://img.youtube.com/vi/${data.video_id}/0.jpg`;
+  now.cover.alt = data.title;
+  now.title.innerText = data.title;
+  now.author.innerText = data.author;
+  now.time.innerText = `${date
     .getUTCHours()
     .toString()
     .padStart(2, '0')}:${date
     .getUTCMinutes()
     .toString()
     .padStart(2, '0')}:${date.getUTCSeconds().toString().padStart(2, '0')}`;
+}
+
+// LOADING DAS INFORMAÇÕES DA MÚSICA ATUAL
+function loadingInfo(playerState) {
+  if (playerState != 1 && playerState != 5 && playerState != 2) {
+    now.loading.classList.add('container__loading--on');
+  } else {
+    now.loading.classList.remove('container__loading--on');
+  }
 }
 
 // RETORNO DE ELEMENTO DOM POR ID
